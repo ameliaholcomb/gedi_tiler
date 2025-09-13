abee2fe4fb901518c73f154d34b3abd7efecdec7
ameliah <amelia.holcomb@gmail.com>
2025-09-12 21:04:08 -0700

    initial commit for DPS run scripts

---
+++ run_gtiler.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env -S bash --login
+set -euo pipefail
+# This script is the one that is called by the DPS.
+# Use this script to prepare input paths for any files
+# that are downloaded by the DPS and outputs that are
+# required to be persisted
+
+# Get current location of build script
+basedir=$(dirname "$(readlink -f "$0")")
+
+# Create output directory to store outputs.
+# The name is output as required by the DPS.
+# Note how we dont provide an absolute path
+# but instead a relative one as the DPS creates
+# a temp working directory for our code.
+
+mkdir -p output
+
+# DPS downloads all files provided as inputs to
+# this directory called input.
+INPUT_DIR=input
+
+# Since we only have one input (the schema) we can list it as below
+schema_path=$(ls -A1 input/)
+echo "Schema path: [${schema_path}]"
+
+# Read the positional argument as defined in the algorithm registration here
+bucket=$1
+prefix=$2
+tile_id=$3
+
+# Is there a better way to pass boolean flags?
+if [ -n "$4" ]; then
+    test="--test"
+else
+    test=""
+fi
+
+# Call the script using the absolute paths
+# Use the updated environment when calling 'conda run'
+# This lets us run the same way in a Terminal as in DPS
+# Any output written to the stdout and stderr streams will be
+# automatically captured and placed in the output dir
+
+conda run --live-stream --name python python ${basedir}/gtiler/setup_database.py --schema_path input/${schema_path} --bucket ${bucket} --prefix ${prefix} --tile_id ${tile_id} ${test}
